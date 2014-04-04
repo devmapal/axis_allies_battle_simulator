bbda5b0329dcb83135c3e0f2ad66bdd4ac3d087c
Fabian Schaffert <devmapal@gmail.com>
2014-04-04 17:21:38 +0200

    Added symbol for run action

--- axis_allies_calc/src/main/java/foss/devmapal/axis_allies_calc/axis_allies_calc/NavigationDrawerFragment.java
+++ axis_allies_calc/src/main/java/foss/devmapal/axis_allies_calc/axis_allies_calc/NavigationDrawerFragment.java
@@ -102,9 +102,8 @@ public class NavigationDrawerFragment extends Fragment {
                 android.R.layout.simple_list_item_activated_1,
                 android.R.id.text1,
                 new String[]{
-                        getString(R.string.title_section1),
-                        getString(R.string.title_section2),
-                        getString(R.string.title_section3),
+                        getString(R.string.title_land_battle),
+                        getString(R.string.title_naval_battle),
                 }));
         mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
         return mDrawerListView;
@@ -247,7 +246,7 @@ public class NavigationDrawerFragment extends Fragment {
             return true;
         }
 
-        if (item.getItemId() == R.id.action_example) {
+        if (item.getItemId() == R.id.action_run) {
             Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
             return true;
         }
